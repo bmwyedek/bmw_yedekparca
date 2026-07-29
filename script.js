d32c0630eb91bdfde64ea16bc841d22455f9fd2a
bmwyedek <erhankucuk0731@gmail.com>
2026-07-30 00:46:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
-const slides = document.querySelectorAll(".slide");
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
 
-let index = 0;
-
-function changeSlide(){
-
-    slides[index].classList.remove("active");
-
-    index++;
-
-    if(index >= slides.length){
-
-        index = 0;
-
-    }
-
-    slides[index].classList.add("active");
-
-}
-
-setInterval(changeSlide,4000);
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
